4244a346c535bc8be35c3257dbf65df05bb0da7f
tirq <tiagogelatti@outlook.com>
2020-01-13 21:06:22 +0100

    Updates

---
+++ precache-manifest.7a279473d079cc9f533c023f56870445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48cdf02a6a1af64218d39d4d264baaec",
+    "url": "/learning-react-the-complete-Guide-burger-builder/index.html"
+  },
+  {
+    "revision": "1b0006846fa42a7ce4d2",
+    "url": "/learning-react-the-complete-Guide-burger-builder/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "e154f4cf7e613bd909ea",
+    "url": "/learning-react-the-complete-Guide-burger-builder/static/js/2.231fd2f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/learning-react-the-complete-Guide-burger-builder/static/js/2.231fd2f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b0006846fa42a7ce4d2",
+    "url": "/learning-react-the-complete-Guide-burger-builder/static/js/main.861c1877.chunk.js"
+  },
+  {
+    "revision": "5ba0d7ede459a1438742",
+    "url": "/learning-react-the-complete-Guide-burger-builder/static/js/runtime-main.7a7dd317.js"
+  }
+]);
