fb9bfba100e088f03cc2a1156fa35a541507b315
tirq <tiagogelatti@outlook.com>
2020-02-05 20:45:20 +0100

    0015-Add static navigation items on the toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Toolbar.module.css';
-import Logo from '../Logo/Logo';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = () => {
     return (
@@ -8,7 +9,7 @@ const toolbar = () => {
             <div>MENU</div>
             <Logo></Logo>
             <nav>
-                ...
+                <NavigationItems/>
             </nav>
         </header>
     );
